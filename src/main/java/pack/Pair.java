7078098685f95c6fc158a888a35691cf844eb318
unknown <admin@VLAN-25-IKK-353.rd.mirea.ru>
2017-02-17 14:30:27 +0300

    Initial commit

---
+++ src/main/java/pack/Pair.java
@@ -0,0 +1,15 @@
+package pack;
+
+/**
+ * Created by admin on 10.02.2017.
+ */
+public class Pair {
+    String key;
+    String value;
+
+    public Pair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
